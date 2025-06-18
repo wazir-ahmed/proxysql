57d7eb85d608799d86be10afab1df2d89ceea244
Wazir Ahmed <wazirahmedf@gmail.com>
2025-06-19 02:50:27 +0530

    fix curl build

--- common_mk/openssl_flags.mk
+++ common_mk/openssl_flags.mk
@@ -20,15 +20,14 @@ ifeq ($(CUSTOM_OPENSSL_PATH),)
         SSL_IDIR := $(shell export PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1; export PKG_CONFIG_ALLOW_SYSTEM_LIBS=1; pkg-config --cflags $(OPENSSL_PACKAGE) | grep -oP "(?<=-I)[^ ]+")
         SSL_LDIR := $(shell pkg-config --variable=libdir $(OPENSSL_PACKAGE))
     endif
-    LIB_SSL_PATH := $(shell find $(SSL_LDIR) -name "libssl.so" 2>/dev/null | head -n 1)
-    LIB_CRYPTO_PATH := $(shell find $(SSL_LDIR) -name "libcrypto.so" 2>/dev/null | head -n 1)
 else
     SSL_IDIR := $(CUSTOM_OPENSSL_PATH)/include
-    SSL_LDIR := $(CUSTOM_OPENSSL_PATH)/lib64
-    LIB_SSL_PATH := $(shell find $(SSL_LDIR) -name "libssl.so" 2>/dev/null | head -n 1)
-    LIB_CRYPTO_PATH := $(shell find $(SSL_LDIR) -name "libcrypto.so" 2>/dev/null | head -n 1)	
+    SSL_LDIR := $(CUSTOM_OPENSSL_PATH)/lib64	
     $(info Using custom OpenSSL path: $(CUSTOM_OPENSSL_PATH))
 endif
+
+LIB_SSL_PATH := $(shell find $(SSL_LDIR) -name "libssl.so" 2>/dev/null | head -n 1)
+LIB_CRYPTO_PATH := $(shell find $(SSL_LDIR) -name "libcrypto.so" 2>/dev/null | head -n 1)
 
 # Check if required flags are set and provide feedback
 ifneq ($(SSL_IDIR),)
